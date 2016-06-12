df713b48e46cbb2b38ed6b96e554142b89f7b67f
Eilidh Hendry <ehendry91@gmail.com>
2016-06-12 15:55:20 +0100

    removing old index function

--- bookmark_manager_app/views.py
+++ bookmark_manager_app/views.py
@@ -3,9 +3,6 @@ from rest_framework import viewsets
 from models import Bookmark
 from serializers import BookmarkSerializer
 
-def index(request):
-    return render(request, 'index.html')
-
 class BookmarkViewSet(viewsets.ModelViewSet):
     """ ViewSet for viewing and editing Bookmark objects """
     queryset = Bookmark.objects.all()
